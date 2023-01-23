c9debfccb5be00b68430ff1f86f91d51208afd27
Indrajeet Singh Badgujar <indrajba@amdocs.com>
2023-01-23 22:57:52 +0530

    svg files are added

---
+++ src/ProtfolioContainer/Home/Profile.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import profilePic from "../../assests/unnamed.webp";
+import "./Profile.scss";
+import "bootstrap/js/src/collapse.js";
+import aboutMeSvg from "../../assests/aboutMe.svg";
+import hireMe from "../../assests/hierMe.svg";
+import { Nav } from "react-bootstrap";
+
+function Profile() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Profile;
