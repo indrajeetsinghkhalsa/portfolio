fe2331914298f5d7c8bee7802ffe29113b5b1671
Indrajeet Singh Badgujar <indrajba@amdocs.com>
2023-01-26 20:18:44 +0530

    BrowserRouter change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import {Route, Routes, BrowserRouter } from "react-router-dom";
+import {Route, Routes, BrowserRouter, HashRouter } from "react-router-dom";
 import "./App.css";
 import { ContactMe } from "./ProtfolioContainer/ContactMe/ContactMe";
 import Profile from "./ProtfolioContainer/Home/Profile";
 import ResponsiveSideMenu from "./ProtfolioContainer/sideMenu/ResponsiveSideMenu";
 
 function App() {
+  /** BrowserRouter is remove because github not support properly 
+   * insted of BrowserRouter using HashRouter to solve the subpath updation
+  */
   return (
     <div className="App">   
-      <BrowserRouter>
+      <HashRouter>
       <ResponsiveSideMenu />
         <Routes>
           <Route exact path="/" element={<Profile/>} />
           <Route exact path="/ContactMe" element={<ContactMe/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
