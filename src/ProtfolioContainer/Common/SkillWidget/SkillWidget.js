7139baf826d29e4b7eddc5c1da7a87085a396428
Indrajeet Singh Badgujar <indrajba@amdocs.com>
2023-04-02 19:49:17 +0530

    svgs added and alignment of svg fixed

--- src/ProtfolioContainer/Common/SkillWidget/SkillWidget.js
+++ src/ProtfolioContainer/Common/SkillWidget/SkillWidget.js
@@ -1,11 +1,11 @@
 import "./SkillWidget.scss";
 
-export const SkillWidget = ({ Icon, title, description, className }) => {
+export const SkillWidget = ({ iconList, title, description, className }) => {
   return (
     <div className="skill col-6 col-lg-3">
       <div className={`item-inner ${className === undefined ? "" : className}`}>
         <div className="item-icon">
-          <Icon />
+          {iconList && iconList.map((Icon) => <Icon />)}
         </div>
         <h3 className="item-title">{title}</h3>
         <div className="item-description">{description}</div>
